dad8dcc4545abe06e075739521ae86c35e7425cb
Ольга Омельчишина <ovomelchishina@edu.hse.ru>
2019-03-12 20:44:49 +0300

    fixed xlsx create

--- main.py
+++ main.py
@@ -571,41 +571,53 @@ def removeUser_TG(login, path='QA.db'):
 import xlsxwriter
 
 def createXLSFileOfQuestions(path='QA.db'):
+    file_name = 'QA.xlsx'
     questions = getListOfQAfromDB(path)
-    workbook = xlsxwriter.Workbook('QA.xlsx')
+    workbook = xlsxwriter.Workbook(file_name)
     worksheet = workbook.add_worksheet(name='Main')
     row = 0
+
+    worksheet.write_string(row, 0, "question\t\t")
+    worksheet.write_string(row, 1, "answer\t\t")
+    worksheet.write_string(row, 2, "zero form question\t\t")
+    row += 1
+
     for _qa in questions:
         worksheet.write_string(row, 0, _qa.question)
         worksheet.write_string(row, 1, _qa.answer)
         worksheet.write_string(row, 2, _qa.null_question)
         row += 1
     workbook.close()
-
-
+    return file_name
 def createXLSFileOfUsers_TG(path='QA.db'):
     users = getUsersFromDB_TG(path)
     file_name = 'users_TG.xlsx'
     workbook = xlsxwriter.Workbook(file_name)
     worksheet = workbook.add_worksheet(name='Main')
     row = 0
+    worksheet.write_string(row, 0, "TG login")
+    worksheet.write_string(row, 1, "Super Admin")
+    row += 1
     for _user in users:
         worksheet.write_string(row, 0, _user.login)
-        worksheet.write_string(row, 1, _user.superUser)
+        worksheet.write_boolean(row, 1, _user.superUser)
         row += 1
     workbook.close()
     return file_name
-
-
 def createXLSFileOfUsers_VK(path='QA.db'):
     users = getUsersFromDB_VK(path)
     file_name = 'users_VK.xlsx'
     workbook = xlsxwriter.Workbook(file_name)
     worksheet = workbook.add_worksheet(name='Main')
     row = 0
+
+    worksheet.write_string(row, 0, "VK id")
+    worksheet.write_string(row, 1, "Super Admin")
+    worksheet.write_string(row, 2, "VK Name")
+    row += 1
     for _user in users:
-        worksheet.write_string(row, 0, _user.id)
-        worksheet.write_string(row, 1, _user.superUser)
+        worksheet.write_number(row, 0, _user.id)
+        worksheet.write_boolean(row, 1, _user.superUser)
         worksheet.write_string(row, 2, _user.flname)
         row += 1
     workbook.close()
@@ -1189,4 +1201,7 @@ tel = TelegramThread()
 vk_admin_list = getUsersFromDB_VK()
 
 #vk.start()
-tel.start()
+#tel.start()
+createXLSFileOfQuestions()
+createXLSFileOfUsers_TG()
+createXLSFileOfUsers_VK()
